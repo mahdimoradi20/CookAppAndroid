94b455c19de912dc858ac0146680a6e0a0aa9791
Mahdi_Moradi <Mahdi_Moradi19@yahoo.com>
2020-10-03 23:24:38 +0330

    Starting work on UI finally :)

--- app/src/main/java/cook/mahdi/moradi/ActivitiesInApp/MainActivity.java
+++ app/src/main/java/cook/mahdi/moradi/ActivitiesInApp/MainActivity.java
@@ -3,9 +3,15 @@ package cook.mahdi.moradi.ActivitiesInApp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.view.animation.BounceInterpolator;
+
+import com.sdsmdg.harjot.rotatingtext.RotatingTextWrapper;
+import com.sdsmdg.harjot.rotatingtext.models.Rotatable;
+
 import cook.mahdi.moradi.R;
 public class MainActivity extends AppCompatActivity {
 
@@ -15,6 +21,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        RotatingTextWrapper rotatingTextWrapper = (RotatingTextWrapper) findViewById(R.id.custom_switcher);
+        rotatingTextWrapper.setSize(35);
+
+        Rotatable rotatable = new Rotatable(Color.parseColor("#FFA036"), 1000, "سریع", "آسان", "بی دردسر");
+        rotatable.setSize(45);
+        rotatable.setInterpolator(new BounceInterpolator());
+        rotatable.setAnimationDuration(500);
+
+        rotatingTextWrapper.setContent("? آشپزی", rotatable);
     }
 
     public void startBtn(View view) {
